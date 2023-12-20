e2fcb89208335f83af268be37181a585f92fe185
rlawndks4 <rlawndks0423@naver.com>
2023-12-20 14:31:21 +0900

    update

--- utils.js/corp-util/bankners.js
+++ utils.js/corp-util/bankners.js
@@ -2,16 +2,12 @@ import axios from 'axios';
 import 'dotenv/config';
 import crypto from 'crypto';
 
-const API_URL = process.env.NODE_ENV == 'production' ? "https://stgapi.bankners.com" : "https://api.bankners.com";
+const API_URL = process.env.NODE_ENV == 'production' ? "https://api.bankners.com" : "https://stgapi.bankners.com";
 
 const makeHeaderData = (dns_data, pay_type, decode_user) => {
     let cur_time = new Date().getTime();
     let req_uniq_no = `${cur_time}${dns_data?.id}${decode_user?.id}`;
-    console.log(dns_data[`${pay_type}_api_id`])
-    console.log(req_uniq_no)
-    console.log(dns_data[`${pay_type}_sign_key`])
     let api_sign_val = crypto.createHash('sha256').update(`${dns_data[`${pay_type}_api_id`]}${req_uniq_no}${dns_data[`${pay_type}_sign_key`]}`).digest('hex');
-    console.log(api_sign_val)
     return {
         'Content-Type': 'application/json',
         'api_id': dns_data[`${pay_type}_api_id`],
@@ -28,7 +24,6 @@ export const banknersApi = {
                     guid: dns_data[`${pay_type}_guid`]
                 }
                 query = new URLSearchParams(query).toString();
-                console.log(query)
                 let result = await axios.get(`${API_URL}/api/user/info?${query}`, {
                     headers: makeHeaderData(dns_data, pay_type, decode_user)
                 })
@@ -40,5 +35,3 @@ export const banknersApi = {
         }
     }
 }
-
-banknersApi.user.info()
