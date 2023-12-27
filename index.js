93992c9052922c90dcf6d5918a4ba664217f4420
rlawndks4 <rlawndks0423@naver.com>
2023-12-27 12:04:10 +0900

    update

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
 });
 let server = undefined
 const HTTP_PORT = 8001;
-const HTTPS_PORT = 8443;
+const HTTPS_PORT = 443;
 if (process.env.NODE_ENV == 'development') {
   server = http.createServer(app).listen(HTTP_PORT, function () {
     console.log("**-------------------------------------**");
