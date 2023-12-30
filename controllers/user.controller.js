8f9474e5c07264a79837b6a026b9309ebfcd7b45
rlawndks4 <rlawndks0423@naver.com>
2023-12-30 19:43:28 +0900

    update

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -233,19 +233,20 @@ const userCtrl = {
                 if (api_result.code != 100) {
                     return response(req, res, -100, (api_result?.message || "서버 에러 발생"), false)
                 }
-                let issued_api_result = await corpApi.vaccount({
-                    pay_type: 'deposit',
-                    decode_dns,
-                    decode_user: ago_user,
-                    guid: ago_user?.guid,
-                })
-                if (issued_api_result.code != 100) {
-                    return response(req, res, -100, (issued_api_result?.message || "서버 에러 발생"), false)
-                }
-                obj['virtual_bank_code'] = issued_api_result.data?.bank_id;
-                obj['virtual_acct_num'] = issued_api_result.data?.virtual_acct_num;
-                obj['virtual_acct_name'] = issued_api_result.data?.virtual_acct_name;
+
             }
+            let issued_api_result = await corpApi.vaccount({
+                pay_type: 'deposit',
+                decode_dns,
+                decode_user: ago_user,
+                guid: ago_user?.guid,
+            })
+            if (issued_api_result.code != 100) {
+                return response(req, res, -100, (issued_api_result?.message || "서버 에러 발생"), false)
+            }
+            obj['virtual_bank_code'] = issued_api_result.data?.bank_id;
+            obj['virtual_acct_num'] = issued_api_result.data?.virtual_acct_num;
+            obj['virtual_acct_name'] = issued_api_result.data?.virtual_acct_name;
             let result = await updateQuery(`${table_name}`, obj, id);
             if (level == 10) {//가맹점
                 let mcht_obj = {
