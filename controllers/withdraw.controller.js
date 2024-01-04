d034f57673aaaf81564828565b3f7eb90cd9555e
rlawndks4 <rlawndks0423@naver.com>
2024-01-04 17:55:08 +0900

    update

--- controllers/withdraw.controller.js
+++ controllers/withdraw.controller.js
@@ -157,6 +157,9 @@ const withdrawCtrl = {
             if (withdraw_amount > mother_account?.real_amount) {
                 return response(req, res, -100, "출금 요청금이 모계좌잔액보다 많습니다.", false)
             }
+            let result = await insertQuery(`${table_name}`, deposit_obj);
+            let withdraw_id = result?.result?.insertId;
+
             let api_move_to_user_amount_result = await corpApi.transfer.pass({
                 pay_type: 'deposit',
                 dns_data: decode_dns,
@@ -169,6 +172,9 @@ const withdrawCtrl = {
             if (api_move_to_user_amount_result.code != 100) {
                 return response(req, res, -100, (api_move_to_user_amount_result?.message || "서버 에러 발생"), api_move_to_user_amount_result?.data)
             }
+            let result2 = await updateQuery(`${table_name}`, {
+                is_pass_confirm: 1,
+            }, withdraw_id);
             let api_withdraw_request_result = await corpApi.user.withdraw.request({
                 pay_type: 'withdraw',
                 dns_data: decode_dns,
@@ -179,8 +185,9 @@ const withdrawCtrl = {
             if (api_withdraw_request_result.code != 100) {
                 return response(req, res, -100, (api_withdraw_request_result?.message || "서버 에러 발생"), api_withdraw_request_result?.data)
             }
-            deposit_obj['trx_id'] = api_withdraw_request_result.data?.tid;
-            let result = await insertQuery(`${table_name}`, deposit_obj);
+            let result3 = await updateQuery(`${table_name}`, {
+                trx_id: api_withdraw_request_result.data?.tid,
+            }, withdraw_id);
             /*
             let trx_id = `${new Date().getTime()}${decode_dns?.id}${user?.id}5`;
             let deposit_obj = {
