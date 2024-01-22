a39c102d9fcc86b9ae8264e72741234cbacbb595
rlawndks4 <rlawndks0423@naver.com>
2024-01-23 00:27:18 +0900

    update

--- utils.js/schedules/index.js
+++ utils.js/schedules/index.js
@@ -1,8 +1,9 @@
 import schedule from 'node-schedule';
+import { pushDepositNoti } from './push-noti.js';
 
 const scheduleIndex = () => {
     schedule.scheduleJob('0 0/1 * * * *', async function () {
-
+        pushDepositNoti();
     })
 }
 
