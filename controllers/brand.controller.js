0cc46fb5f7540168b4fe9aee540794a66fda5f66
rlawndks4 <rlawndks0423@naver.com>
2024-02-05 02:26:34 +0900

    update

--- controllers/brand.controller.js
+++ controllers/brand.controller.js
@@ -87,7 +87,7 @@ const brandCtrl = {
                 default_deposit_fee, default_withdraw_fee, head_office_fee = 0, deposit_head_office_fee = 0, withdraw_head_office_fee = 0, default_withdraw_max_price = 0, withdraw_type = 0, withdraw_fee_type = 0,
                 is_use_telegram_bot = 0, telegram_bot_token = "", telegram_bot_id = "", is_use_otp = 0, otp_token = "",
                 is_use_fee_operator = 1, is_use_deposit_operator = 0, is_use_withdraw_operator = 0,
-                is_use_corp_account = 0, corp_account_corp_type = 0, is_can_add_deposit = 0, is_use_asapmall_noti = 0,
+                is_use_corp_account = 0, corp_account_corp_type = 0, is_can_add_deposit = 0, is_use_asapmall_noti = 0, asapmall_dns = "",
             } = req.body;
             let files = settingFiles(req.files);
             let obj = {
@@ -97,7 +97,7 @@ const brandCtrl = {
                 default_deposit_fee, default_withdraw_fee, head_office_fee, deposit_head_office_fee, withdraw_head_office_fee, default_withdraw_max_price, withdraw_type, withdraw_fee_type,
                 is_use_telegram_bot, telegram_bot_token, telegram_bot_id, is_use_otp, otp_token,
                 is_use_fee_operator, is_use_deposit_operator, is_use_withdraw_operator,
-                is_use_corp_account, corp_account_corp_type, is_can_add_deposit, is_use_asapmall_noti,
+                is_use_corp_account, corp_account_corp_type, is_can_add_deposit, is_use_asapmall_noti, asapmall_dns,
             };
             obj['theme_css'] = JSON.stringify(obj.theme_css);
             obj['setting_obj'] = JSON.stringify(obj.setting_obj);
@@ -152,7 +152,7 @@ const brandCtrl = {
                 deposit_noti_url, withdraw_noti_url, withdraw_fail_noti_url, api_url, withdraw_type = 0, withdraw_fee_type = 0,
                 is_use_telegram_bot = 0, telegram_bot_token = "", telegram_bot_id = "", is_use_otp = 0, otp_token = "",
                 is_use_fee_operator = 1, is_use_deposit_operator = 0, is_use_withdraw_operator = 0,
-                is_use_corp_account = 0, corp_account_corp_type = 0, is_can_add_deposit = 0, is_use_asapmall_noti = 0,
+                is_use_corp_account = 0, corp_account_corp_type = 0, is_can_add_deposit = 0, is_use_asapmall_noti = 0, asapmall_dns = "",
                 guid = "",
             } = req.body;
             const { id } = req.params;
@@ -168,7 +168,7 @@ const brandCtrl = {
                 default_deposit_fee, default_withdraw_fee, head_office_fee, deposit_head_office_fee, withdraw_head_office_fee, default_withdraw_max_price, api_url, withdraw_type, withdraw_fee_type,
                 is_use_telegram_bot, telegram_bot_token, telegram_bot_id, is_use_otp, otp_token,
                 is_use_fee_operator, is_use_deposit_operator, is_use_withdraw_operator,
-                is_use_corp_account, corp_account_corp_type, is_can_add_deposit, is_use_asapmall_noti,
+                is_use_corp_account, corp_account_corp_type, is_can_add_deposit, is_use_asapmall_noti, asapmall_dns,
             };
             obj['theme_css'] = JSON.stringify(obj.theme_css);
             obj['setting_obj'] = JSON.stringify(obj.setting_obj);
--- utils.js/schedules/push-asapmall.js
+++ utils.js/schedules/push-asapmall.js
@@ -1,15 +1,54 @@
 import 'dotenv/config';
 import { pool } from '../../config/db.js';
+import axios from 'axios';
+import { updateQuery } from '../query-util.js';
 
 export const pushAsapMall = async () => {
-    let sql = ` SELECT * FROM deposits `;
+    let sql = ` SELECT deposits.*, brands.asapmall_dns FROM deposits `;
     sql += ` LEFT JOIN brands ON deposits.brand_id=brands.id `;
     sql += ` WHERE brands.is_use_asapmall_noti=1 AND deposits.send_asapmall_noti=5 AND pay_type IN (0, 5, 20) `;
     sql += ` ORDER BY deposits.id ASC `;
     let data = await pool.query(sql);
     data = data?.result;
     for (var i = 0; i < data.length; i++) {
-        console.log(data[i])
+        let {
+            asapmall_dns,
+            pay_type,
+            amount,
+            withdraw_fee,
+            trx_id,
+            deposit_acct_name,
+            settle_acct_name,
+            id,
+        } = data[i];
+        if (amount > 0 || amount < 0) {
+            let obj = {
+                dns: asapmall_dns,
+                amount: amount,
+                tid: trx_id,
+            };
+            if (pay_type == 0) {
+                obj['pay_type'] = 'deposit';
+                obj['acct_name'] = deposit_acct_name;
+
+            } else if (pay_type == 5) {
+                obj['pay_type'] = 'withdraw';
+                obj['amount'] = amount + withdraw_fee;
+                obj['acct_name'] = settle_acct_name;
+
+
+            } else if (pay_type == 20) {
+                obj['pay_type'] = 'return';
+                obj['amount'] = amount + withdraw_fee;
+                obj['acct_name'] = settle_acct_name;
+
+            }
+            let { data: response } = await axios.post(`${process.env.SHOPPING_MALL_BACK_URL}/api/pays/virtual-acct/noti`, obj);
+            if (response?.result > 0) {
+                let result = await updateQuery(`deposits`, {
+                    send_asapmall_noti: 0,
+                }, id)
+            }
+        }
     }
 }
-pushAsapMall();
