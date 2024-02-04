0a16ea870758257205a60ef148bc460f3f3ddbd5
rlawndks4 <rlawndks0423@naver.com>
2024-02-04 23:51:57 +0900

    update

--- utils.js/schedules/push-asapmall.js
+++ utils.js/schedules/push-asapmall.js
@@ -4,9 +4,12 @@ import { pool } from '../../config/db.js';
 export const pushAsapMall = async () => {
     let sql = ` SELECT * FROM deposits `;
     sql += ` LEFT JOIN brands ON deposits.brand_id=brands.id `;
-    sql += ` WHERE brands.is_use_asapmall_noti=1 AND deposits.send_asapmall_noti=5 `;
+    sql += ` WHERE brands.is_use_asapmall_noti=1 AND deposits.send_asapmall_noti=5 AND pay_type IN (0, 5, 20) `;
     sql += ` ORDER BY deposits.id ASC `;
     let data = await pool.query(sql);
     data = data?.result;
-    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+        console.log(data[i])
+    }
 }
+pushAsapMall();
