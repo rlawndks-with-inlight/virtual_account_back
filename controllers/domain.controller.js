e8d2c0eada47388d457a08e775f9e0a2ffc40d1d
rlawndks4 <rlawndks0423@naver.com>
2024-02-29 01:24:19 +0900

    update

--- controllers/domain.controller.js
+++ controllers/domain.controller.js
@@ -67,7 +67,6 @@ const domainCtrl = {
             brand['bizppurio_obj'] = JSON.parse(brand?.bizppurio_obj ?? '{}');
 
             brand['operator_list'] = getOperatorList(brand);
-            console.log(brand)
             let brands = await pool.query(`SELECT id, parent_id FROM brands `);
             brands = brands?.result;
             let childrens = findChildIds(brands, brand?.id);
