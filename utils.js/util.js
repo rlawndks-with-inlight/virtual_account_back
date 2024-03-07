7b5f5b676897df37785b75dee9af2a21882d8a73
rlawndks4 <rlawndks0423@naver.com>
2024-03-08 01:04:24 +0900

    update

--- utils.js/util.js
+++ utils.js/util.js
@@ -130,7 +130,9 @@ export const response = async (req, res, code, message, data) => { //응답 포�
     }
     const decode_user = checkLevel(req.cookies.token, 0, res)
     const decode_dns = checkDns(req.cookies.dns, 0)
-    let save_log = await logRequestResponse(req, resDict, decode_user, decode_dns);
+    if (req.originalUrl?.includes('/auth') || req.originalUrl?.includes('/withdraw')) {
+        let save_log = await logRequestResponse(req, resDict, decode_user, decode_dns);
+    }
     if (req?.IS_RETURN) {
         return resDict;
     } else {
