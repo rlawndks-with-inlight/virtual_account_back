4e8eccc850448c78f7d179dc9097d73f5d08c9ff
rlawndks4 <rlawndks0423@naver.com>
2024-03-08 20:40:52 +0900

    update

--- utils.js/schedules/push-asapmall.js
+++ utils.js/schedules/push-asapmall.js
@@ -81,6 +81,7 @@ export const pushAsapMall = async (return_moment = "") => {
                     obj['acct_name'] = settle_acct_name;
                 }
                 sendNotiPushAsapMall(data[i], obj)
+                await new Promise((r) => setTimeout(r, 1000));
             }
         }
     } catch (err) {
