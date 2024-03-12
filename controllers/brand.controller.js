a24031e3d368ddcfce355ef3d85d89b7b91efd1a
rlawndks4 <rlawndks0423@naver.com>
2024-03-13 04:59:03 +0900

    update

--- controllers/brand.controller.js
+++ controllers/brand.controller.js
@@ -213,7 +213,6 @@ const brandCtrl = {
                     push_url: deposit_noti_url,
                     encr_yn: 'N',
                 })
-                console.log(api_result)
                 if (api_result.code == 100) {
                     obj['deposit_noti_url'] = deposit_noti_url;
                 }
@@ -228,8 +227,6 @@ const brandCtrl = {
                     push_url: withdraw_noti_url,
                     encr_yn: 'N',
                 })
-                console.log(api_result)
-
                 if (api_result.code == 100) {
                     obj['withdraw_noti_url'] = withdraw_noti_url;
                 }
@@ -244,7 +241,6 @@ const brandCtrl = {
                     push_url: withdraw_fail_noti_url,
                     encr_yn: 'N',
                 })
-                console.log(api_result)
                 if (api_result.code == 100) {
                     obj['withdraw_fail_noti_url'] = withdraw_fail_noti_url;
                 }
