96bf75e716fb2c410706b6bf9b4764dd7fc70251
rlawndks4 <rlawndks0423@naver.com>
2024-03-14 22:51:26 +0900

    update

--- utils.js/limiter/index.js
+++ utils.js/limiter/index.js
@@ -1,8 +1,8 @@
 import rateLimit from 'express-rate-limit';
 
 const limiter = rateLimit({
-    windowMs: 1000 * 5, //1초 ... 15 * 60 * 1000 15분
-    max: 5,
+    windowMs: 1000 * 60, //1초 ... 15 * 60 * 1000 15분
+    max: 60,
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
     message: {
