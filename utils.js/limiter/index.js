fd8416cdb55f0dad59c71b5813fe4e5307f71329
rlawndks4 <rlawndks0423@naver.com>
2024-03-14 22:51:16 +0900

    update

--- utils.js/limiter/index.js
+++ utils.js/limiter/index.js
@@ -1,15 +1,17 @@
 import rateLimit from 'express-rate-limit';
 
 const limiter = rateLimit({
-    windowMs: 1000 * 60, //1초 ... 15 * 60 * 1000 15분
-    max: 60,
+    windowMs: 1000 * 5, //1초 ... 15 * 60 * 1000 15분
+    max: 5,
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
     message: {
         rate_limiter: 1
     },
     handler: function (req, res) {
-        res.status(429).send('요청이 너무 많습니다. 잠시 후 다시 시도해주세요.');
+        res.status(429).send({
+            rate_limiter: 1
+        });
     }
 });
 
