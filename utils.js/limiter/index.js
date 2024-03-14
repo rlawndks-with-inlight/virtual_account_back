cef45dc55d9f0dbd949493625906c4df4e3c7691
rlawndks4 <rlawndks0423@naver.com>
2024-03-14 22:46:50 +0900

    update

--- utils.js/limiter/index.js
+++ utils.js/limiter/index.js
@@ -7,6 +7,9 @@ const limiter = rateLimit({
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
     message: {
         rate_limiter: 1
+    },
+    handler: function (req, res) {
+        res.status(429).send('요청이 너무 많습니다. 잠시 후 다시 시도해주세요.');
     }
 });
 
