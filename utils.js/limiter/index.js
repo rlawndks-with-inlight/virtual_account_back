a39dc6cb96459b129b966e64dc140c6b0e09a6d1
rlawndks4 <rlawndks0423@naver.com>
2024-03-14 23:00:43 +0900

    update

--- utils.js/limiter/index.js
+++ utils.js/limiter/index.js
@@ -1,4 +1,5 @@
 import rateLimit from 'express-rate-limit';
+import { getReqIp } from '../util.js';
 
 const limiter = rateLimit({
     windowMs: 1000 * 60, //1초 ... 15 * 60 * 1000 15분
@@ -9,6 +10,8 @@ const limiter = rateLimit({
         rate_limiter: 1
     },
     handler: function (req, res) {
+        let ip = getReqIp(req);
+        console.log(ip)
         res.status(429).send({
             rate_limiter: 1
         });
