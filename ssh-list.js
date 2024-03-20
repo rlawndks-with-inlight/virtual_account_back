6889ef4e97cdd4adb3e73ec3ccc6bce0fa75a4eb
rlawndks4 <rlawndks0423@naver.com>
2024-03-20 15:32:30 +0900

    update

--- ssh-list.js
+++ ssh-list.js
@@ -1,10 +1,5 @@
 import fs from 'fs';
 
 export const ssh_list = [
-    {
-        host: 'your_ssh_host',
-        port: 22, // 기본 포트는 22번입니다.
-        username: 'your_username',
-        privateKey: fs.readFileSync('C:/Users/pc/Desktop/project/virtual-account/keypair/lux/lux-pay-2.pem')
-    }
+
 ]
