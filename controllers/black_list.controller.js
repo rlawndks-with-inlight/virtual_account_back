ae762580eee4f8932aaf945642ca8741241fc341
rlawndks4 <rlawndks0423@naver.com>
2024-04-20 21:17:20 +0900

    update

--- controllers/black_list.controller.js
+++ controllers/black_list.controller.js
@@ -12,10 +12,12 @@ const blackListCtrl = {
     list: async (req, res, next) => {
         try {
             let is_manager = await checkIsManagerUrl(req);
-            const decode_user = await checkLevel(req.cookies.token, 40, req);
+            const decode_user = await checkLevel(req.cookies.token, 10, req);
             const decode_dns = checkDns(req.cookies.dns);
             const { search } = req.query;
-
+            if (!decode_user) {
+                return lowLevelException(req, res);
+            }
             let search_columns = [
                 `mchts.user_name`,
                 `mchts.nickname`,
@@ -30,6 +32,9 @@ const blackListCtrl = {
             let sql = `SELECT ${process.env.SELECT_COLUMN_SECRET} FROM ${table_name} `;
             sql += ` LEFT JOIN users AS mchts ON ${table_name}.mcht_id=mchts.id `;
             sql += ` WHERE ${table_name}.brand_id=${decode_dns?.id} `;
+            if (decode_user?.level < 40) {
+                sql += ` AND ${table_name}.mcht_id=${decode_user?.id} `;
+            }
             if (search) {
                 sql += makeSearchQuery(search_columns, search);
             }
