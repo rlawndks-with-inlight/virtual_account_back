5bdc19ada1fcc77f60518fcd8bf72df3f7dce609
rlawndks4 <rlawndks0423@naver.com>
2024-05-06 00:22:23 +0900

    update

--- utils.js/schedules/push-noti.js
+++ utils.js/schedules/push-noti.js
@@ -11,6 +11,7 @@ export const pushDepositNoti = async () => {
         sql += ` LEFT JOIN users ON deposits.mcht_id=users.id `;
         sql += ` WHERE deposits.deposit_noti_status=5 `;
         sql += ` AND CHAR_LENGTH(users.deposit_noti_url) > 0 `;
+        sql += ` AND users.is_delete=0 `;
         sql += ` ORDER BY id ASC `;
         let data = await pool.query(sql);
         data = data?.result;
