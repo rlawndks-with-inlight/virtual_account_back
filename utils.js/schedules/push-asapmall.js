4b74f622ffc476de8094920035574fef8bb6054b
rlawndks4 <rlawndks0423@naver.com>
2024-06-12 03:41:55 +0900

    update

--- utils.js/schedules/push-asapmall.js
+++ utils.js/schedules/push-asapmall.js
@@ -143,7 +143,6 @@ export const pushAsapMall = async (return_moment = "") => {
         console.log(err);
     }
 }
-pushAsapMall(':00:00')
 const sendNotiPushAsapMall = async (data, obj, products = []) => {
     try {
         let {
