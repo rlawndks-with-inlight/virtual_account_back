3bc314cbd3fdad54f65e864d1eb047bbf85aa661
rlawndks4 <rlawndks0423@naver.com>
2024-08-02 16:22:13 +0900

    update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -90,7 +90,7 @@ const authCtrl = {
                 let add_login_fail_count = await updateQuery(`users`, login_fail_obj, user?.id);
                 return response(req, res, -100, err_message, {});
             }
-            if (decode_dns?.is_use_otp == 1 && user?.level < 45) {
+            if (dns_data?.is_use_otp == 1 && user?.level < 45) {
                 let otp_token = '';
                 if (!otp_num) {
                     return response(req, res, -100, "OTP번호를 입력해주세요.", {})
