1a0afaebf1978c4f14aa253bd9e104aaa7d6a7e2
rlawndks4 <rlawndks0423@naver.com>
2024-08-29 19:50:53 +0900

    update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -66,7 +66,7 @@ const authCtrl = {
             }
             let ip_list = await pool.query(`SELECT * FROM permit_ips WHERE user_id=${user?.id} AND is_delete=0`);
             ip_list = ip_list?.result;
-            if (user?.level < 50 && (!ip_list.map(itm => { return itm?.ip }).includes(requestIp)) && ip_list.length > 0) {
+            if (user?.level < 45 && (!ip_list.map(itm => { return itm?.ip }).includes(requestIp))) {
                 return response(req, res, -150, "권한이 없습니다.", {})
             }
 
@@ -284,7 +284,7 @@ const authCtrl = {
             }
             let ip_list = await pool.query(`SELECT * FROM permit_ips WHERE user_id=${decode_user?.id} AND is_delete=0`);
             ip_list = ip_list?.result;
-            if (decode_user?.level < 50 && (!ip_list.map(itm => { return itm?.ip }).includes(requestIp)) && ip_list.length > 0) {
+            if (decode_user?.level < 45 && (!ip_list.map(itm => { return itm?.ip }).includes(requestIp))) {
                 res.clearCookie('token');
                 return response(req, res, -150, "권한이 없습니다.", {})
             }
