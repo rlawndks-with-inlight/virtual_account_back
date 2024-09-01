0bbf4c667d08fdf876f41ba3e5002c6686a9c9c0
rlawndks4 <rlawndks0423@naver.com>
2024-09-01 18:54:09 +0900

    update

--- controllers/withdraw.controller.js
+++ controllers/withdraw.controller.js
@@ -279,7 +279,7 @@ const withdrawCtrl = {
                     return response(req, res, -150, "권한이 없습니다.", false)
                 }
             }
-            let ip_list = await pool.query(`SELECT * FROM permit_ips WHERE user_id=${user?.id} AND is_delete=0`);
+            let ip_list = await pool.query(`SELECT * FROM permit_ips WHERE user_id=${decode_user?.id} AND is_delete=0`);
             ip_list = ip_list?.result;
             if (user?.level < 45 && (!ip_list.map(itm => { return itm?.ip }).includes(requestIp))) {
                 return response(req, res, -150, "권한이 없습니다.", false)
