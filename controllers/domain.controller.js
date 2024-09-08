ad096bebef202e17947052e195295df90fca18d7
rlawndks4 <rlawndks0423@naver.com>
2024-09-09 04:11:28 +0900

    update

--- controllers/domain.controller.js
+++ controllers/domain.controller.js
@@ -65,7 +65,7 @@ const domainCtrl = {
                 ]
                 let brand_sql = ` SELECT ${columns.join()} FROM brands `;
                 brand_sql += ` WHERE (dns='${dns}' OR admin_dns='${dns}') AND is_delete=0 `;
-                let brand = await pool.query(brand_sql);
+                brand = await pool.query(brand_sql);
                 if (brand?.result.length == 0) {
                     return response(req, res, -120, "등록된 도메인이 아닙니다.", false)
                 }
