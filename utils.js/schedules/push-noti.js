fc8bd761c26f3f0fdd3a0d72f8e80a77bfa46fe6
rlawndks-with-inlight <wndks4@gmail.com>
2024-12-10 21:25:08 +0900

    update

--- utils.js/schedules/push-noti.js
+++ utils.js/schedules/push-noti.js
@@ -12,17 +12,17 @@ export const pushDepositNoti = async () => {
         let user_ids = users.map(el => { return el?.id });
         let columns = [
             `deposits.id`,
-            `deposits.user_id`,
+            `deposits.mcht_id`,
             `deposits.deposit_noti_obj`,
         ]
         let sql = `SELECT ${columns.join()} FROM deposits `
         sql += ` WHERE deposits.deposit_noti_status=5 `;
-        sql += ` AND deposits.user_id IN (${user_ids.join()}) `;
+        sql += ` AND deposits.mcht_id IN (${user_ids.join()}) `;
         sql += ` ORDER BY id ASC `;
         let data = await pool.query(sql);
         data = data?.result;
         for (var i = 0; i < data.length; i++) {
-            sendNotiPush(_.find(users, { id: data[i]?.user_id }), 'deposit', JSON.parse(data[i]?.deposit_noti_obj ?? '{}'), data[i]?.id);
+            sendNotiPush(_.find(users, { id: data[i]?.mcht_id }), 'deposit', JSON.parse(data[i]?.deposit_noti_obj ?? '{}'), data[i]?.id);
         }
     } catch (err) {
         console.log(err);
