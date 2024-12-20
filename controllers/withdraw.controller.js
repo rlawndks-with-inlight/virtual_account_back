3a300a8381f2e295526908df63fc62b64b0e427f
rlawndks-with-inlight <wndks4@gmail.com>
2024-12-20 13:44:54 +0900

    update

--- controllers/withdraw.controller.js
+++ controllers/withdraw.controller.js
@@ -465,7 +465,7 @@ const withdrawCtrl = {
             if (!withdraw) {
                 return response(req, res, -100, "잘못된 출금 입니다.", false)
             }
-            if (withdraw?.is_pass_confirm == 1) {
+            if (withdraw?.is_pass_confirm == 1 || withdraw?.is_withdraw_hold != 1) {
                 return response(req, res, -100, "이미 허용된 출금입니다.", false)
             }
             let trx_id = withdraw?.trx_id;
