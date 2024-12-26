296b0c0fc1bd89ccfb849a201bb98c6c24cfbcd5
rlawndks-with-inlight <wndks4@gmail.com>
2024-12-27 03:27:37 +0900

    update

--- utils.js/util.js
+++ utils.js/util.js
@@ -192,7 +192,7 @@ export const response = async (req, res, code, message, data) => { //응답 포�
     }
     const decode_user = await checkLevel(req.cookies.token, 0, req, true)
     const decode_dns = checkDns(req.cookies.dns, 0)
-    if (req.originalUrl?.includes('/auth') || req.method == 'DELETE' || req.method == 'POST' || req.method == 'PUT' || req.query?.page_size >= 500) {
+    if (req.originalUrl?.includes('/auth') || req.method == 'DELETE' || req.method == 'POST' || req.method == 'PUT' || req.query?.page_size >= 500 || code < 0) {
         let save_log = await logRequestResponse(req, resDict, decode_user, decode_dns);
     }
     if (req?.IS_RETURN) {
