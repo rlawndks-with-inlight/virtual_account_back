5c321ee3aec61087b2aaa742f0f464c1f0ef8203
rlawndks-with-inlight <wndks4@gmail.com>
2025-01-24 23:33:25 +0900

    update

--- utils.js/corp-util/korea-pay-system.js
+++ utils.js/corp-util/korea-pay-system.js
@@ -300,7 +300,6 @@ export const koreaPaySystemApi = {
                 let { data: result } = await axios.post(`${API_URL}/api/settle/transfer`, query, {
                     headers: makeHeaderData(dns_data, pay_type)
                 });
-                console.log(result)
                 if (result?.result?.resultCd != '0000') {
                     return {
                         code: -100,
