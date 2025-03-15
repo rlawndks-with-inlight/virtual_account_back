f81048ab58db4bbe181e863ab82a91645bdd511a
rlawndks-with-inlight <wndks4@gmail.com>
2025-03-16 00:55:43 +0900

    update

--- controllers/deposit.controller.js
+++ controllers/deposit.controller.js
@@ -157,7 +157,7 @@ const depositCtrl = {
                 if (s_dt) {
                     where_sql += ` AND ${table_name}.created_at >= '${s_dt} 00:00:00' `;
                 }
-                if (e_dt && e_dt < returnMoment().substring(0, 10)) {
+                if (e_dt) {
                     where_sql += ` AND ${table_name}.created_at <= '${e_dt} 23:59:59' `;
                 }
             }
@@ -170,14 +170,6 @@ const depositCtrl = {
                     where_sql += ` AND ${table_name}.sales${sales_num}_id=${decode_user?.id} `;
                 }
             }
-            if (req.query?.mcht_id > 0) {
-                where_sql += ` AND ${table_name}.mcht_id=${req.query?.mcht_id} `;
-            }
-            for (var i = 0; i < operator_list.length; i++) {
-                if (req.query[`sales${operator_list[i]?.num}_id`] > 0) {
-                    where_sql += ` AND ${table_name}.sales${operator_list[i]?.num}_id=${req.query[`sales${operator_list[i]?.num}_id`]} `;
-                }
-            }
             if (is_mother) {
                 where_sql += ` AND (${table_name}.amount > 0 OR ${table_name}.amount < 0) `
             } else {
@@ -185,6 +177,14 @@ const depositCtrl = {
             }
             if (pay_type) {
                 where_sql += ` AND ${table_name}.pay_type=${pay_type} `
+            }
+            if (req.query?.mcht_id > 0) {
+                where_sql += ` AND ${table_name}.mcht_id=${req.query?.mcht_id} `;
+            }
+            for (var i = 0; i < operator_list.length; i++) {
+                if (req.query[`sales${operator_list[i]?.num}_id`] > 0) {
+                    where_sql += ` AND ${table_name}.sales${operator_list[i]?.num}_id=${req.query[`sales${operator_list[i]?.num}_id`]} `;
+                }
             }
             if (is_cancel) {
                 where_sql += ` AND ${table_name}.is_cancel=${is_cancel} `
