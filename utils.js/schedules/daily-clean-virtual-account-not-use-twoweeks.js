d651ca7a5f864052cddf338ff69658d52ddc757d
rlawndks-with-inlight <wndks4@gmail.com>
2025-03-20 02:08:30 +0900

    update

--- utils.js/schedules/daily-clean-virtual-account-not-use-twoweeks.js
+++ utils.js/schedules/daily-clean-virtual-account-not-use-twoweeks.js
@@ -13,29 +13,32 @@ export const onDailyCleanVirtualAccountNotUseTwoWeeks = async (return_moment) =>
         let left_2_weeks_deposit = await readPool.query(`SELECT id, created_at FROM deposits WHERE created_at >= CURDATE() - INTERVAL 14 DAY ORDER BY id ASC LIMIT 1`);
         left_2_weeks_deposit = left_2_weeks_deposit[0][0];
         for (var i = 0; i < brands.length; i++) {
-            if (brands[i]?.deposit_corp_type == 6) {//코리아아
+            if (brands[i]?.deposit_corp_type == 6) {//코리아
                 let result = await onProcessClean(brands[i], left_2_weeks_deposit);
-                break;
             }
         }
     } catch (err) {
         console.log(err);
     }
 }
-onDailyCleanVirtualAccountNotUseTwoWeeks('02:00:')
+
 const onProcessClean = async (brand = {}, left_2_weeks_deposit = {}) => {
-    let not_use_virtual_accounts = await readPool.query(`SELECT * FROM virtual_accounts WHERE brand_id=${brand?.id} AND is_delete=0 AND id NOT IN (SELECT virtual_account_id FROM deposits WHERE brand_id=${brand?.id} AND id >=${left_2_weeks_deposit?.id} AND virtual_account_id > 0) ORDER BY id ASC`);
-    not_use_virtual_accounts = not_use_virtual_accounts[0];
-    for (var i = 0; i < not_use_virtual_accounts.length / 100; i++) {
-        let process_list = not_use_virtual_accounts.slice(i * 100, (i + 1) * 100);
-        let result_list = [];
-        for (var j = 0; j < process_list.length; j++) {
-            result_list.push(virtualAccountCtrl.remove({ params: { id: process_list[j]?.id }, IS_RETURN: true, dns_data: brand }, {}));
+    try {
+        let not_use_virtual_accounts = await readPool.query(`SELECT * FROM virtual_accounts WHERE brand_id=${brand?.id} AND is_delete=0 AND id NOT IN (SELECT virtual_account_id FROM deposits WHERE brand_id=${brand?.id} AND id >=${left_2_weeks_deposit?.id} AND virtual_account_id > 0) ORDER BY id ASC`);
+        not_use_virtual_accounts = not_use_virtual_accounts[0];
+        for (var i = 0; i < not_use_virtual_accounts.length / 100; i++) {
+            let process_list = not_use_virtual_accounts.slice(i * 100, (i + 1) * 100);
+            let result_list = [];
+            for (var j = 0; j < process_list.length; j++) {
+                result_list.push(virtualAccountCtrl.remove({ params: { id: process_list[j]?.id }, IS_RETURN: true, dns_data: brand }, {}));
+            }
+            for (var j = 0; j < result_list.length; j++) {
+                await result_list[j];
+            }
+            let result = (await when(result_list));
         }
-        for (var j = 0; j < result_list.length; j++) {
-            await result_list[j];
-        }
-        let result = (await when(result_list));
-        break;
+    } catch (err) {
+        console.log(err);
     }
+
 }
