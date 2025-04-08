81e750a2facee5256274cf03c08e64e94e849f4e
rlawndks-with-inlight <wndks4@gmail.com>
2025-04-09 01:07:05 +0900

    update

--- telegram/index.js
+++ telegram/index.js
@@ -1,6 +1,6 @@
 import TelegramBot from "node-telegram-bot-api"
 
-export const sendTelegramBot = (dns_data = {}, message, ids = []) => {
+export const sendTelegramBot = async (dns_data = {}, message, ids = []) => {
     try {
         if (dns_data?.is_use_telegram_bot == 1) {
             let token = dns_data?.telegram_bot_token;
@@ -8,6 +8,7 @@ export const sendTelegramBot = (dns_data = {}, message, ids = []) => {
             const telebot = new TelegramBot(token);
             for (var i = 0; i < ids.length; i++) {
                 telebot.sendMessage(ids[i], message);
+                await new Promise((r) => setTimeout(r, 1000));
             }
         }
     } catch (err) {
